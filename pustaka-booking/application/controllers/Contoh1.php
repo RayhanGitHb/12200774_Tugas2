9a9ffe75e98dbe35ea8b4e7025207ea3f354ad29
Rayhan <90813949+RayhanGitHb@users.noreply.github.com>
2022-03-15 15:20:41 +0700

    Fix to Web Prog 3

--- pustaka-booking/application/controllers/Contoh1.php
+++ pustaka-booking/application/controllers/Contoh1.php
@@ -1,14 +1,12 @@
 <?php
 class Contoh1 extends CI_Controller
 {
-	public function index()
-	{
-		echo "<h1>Perkenalkan</h1>";
-		echo "Nama Saya Rayhan
-			Saya Tinggal di daerah Mangunreja
-			Olah raga yang saya sukai adalah
-			Basket.
-			No. Hp : 089659909985
-			Alamat : Kp. Daramaga, Ds. Salebu, Kec. Mangunreja, Kab. Tasikmalaya";
-	}
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Imam Nawawi
+            Saya tingga di daerah Ciputat
+            olah raga yang saya sukai adalah
+            Bulutangkis";
+    }
 }
